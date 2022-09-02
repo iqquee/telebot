54e00d6dcbd02bcf03fb2726ff469beabcbc86e0
hisyntax <hisyntax0@gmail.com>
2022-09-02 13:22:15 +0100

    welcome message to new joined users

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env not found")
+	}
+
+	token := os.Getenv("TOKEN")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			wc := update.Message.NewChatMembers
+			if wc != nil {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "welcome to this group")
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+			}
+
+			// msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			// msg.ReplyToMessageID = update.Message.MessageID
+
+			// bot.Send(msg)
+		}
+	}
+}
