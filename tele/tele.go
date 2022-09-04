43f8c87ff6a64b85fc723e0481fb1608396f621c
hisyntax <hisyntax0@gmail.com>
2022-09-04 02:26:29 +0100

    new featured implemented

--- tele/tele.go
+++ tele/tele.go
@@ -62,83 +62,130 @@ func Bot() {
 	for update := range updates {
 		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-			up := update.Message
-			fmt.Printf("This is the received message... %v\n", up)
 
-			//welcome new users
-			wc := update.Message.NewChatMembers
-			if wc != nil {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "welcome to test-bot group. Please 30 people to be able to send messages in the group")
-				msg.ReplyToMessageID = update.Message.MessageID
+			// THESE APPLIES TO EVERYONE IN THE GROUP EXCEPT THE ADMIN OF THE GROUP
+			foundUser := update.Message.From.UserName
+			if foundUser != os.Getenv("USER_NAME") {
 
-				bot.Send(msg)
+				//welcome new users
+				wc := update.Message.NewChatMembers
+				if wc != nil {
+					var byt []map[string]string
 
-				//new users should not be able to send messages to the group until they add 30 more persons to the group
+					jsonM, _ := json.Marshal(wc)
 
-				// check if the user was already added to the group before
+					json.Unmarshal(jsonM, &byt)
+					for val := range byt {
+						if byt[val]["username"] != "" {
+							username := byt[val]["username"]
+							sendMsg := fmt.Sprintf("@%s welcome to test-bot group, we catch fun here :)", username)
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, sendMsg)
 
-			}
+							bot.Send(msg)
+						}
+					}
 
-			//check if the recently added user already exists in the datatase
-			filter := bson.M{"from.username": update.Message.From.UserName}
-			res, _ := database.GetMongoDoc(database.UserCollection, filter)
+					// check if the user was already added to the group before
 
-			//loop through the object to get the username of the just added user
-			var byt []map[string]interface{}
-			arr := res.NewChatMembers
-			jm, _ := json.Marshal(arr)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
-			json.Unmarshal(jm, &byt)
-			// ; err != nil {
-			// 	fmt.Println(err)
-			// }
+				}
 
-			//add _ number users to the group before being able to send messages to the group
-			var addedUsers database.AddedUsers
-			jsonr, _ := json.Marshal(up)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
-			json.Unmarshal(jsonr, &addedUsers)
-			// ; err != nil {
-			// 	fmt.Println(err)
-			// }
+				// if update.Message != nil {
+				//check if user is not removed from the group and user is not added to the group
+				//every other messages should be deleted
 
-			var newUser []map[string]interface{}
-			addedUserR := addedUsers.NewChatMembers
-			addedUserjson, _ := json.Marshal(addedUserR)
-			// if err != nil {
-			// 	fmt.Println(err)
-			// }
-			json.Unmarshal(addedUserjson, &newUser)
-			//  err != nil {
-			// 	fmt.Println(err)
-			// }
+				// removeUser := update.Message.LeftChatMember.UserName // get the removed user
+				// var addUser bool
+				// fmt.Printf("%v.....\n", removeUser)
+				// fmt.Printf("%v.....\n", addUser)
 
-			//so long newchatmembers object is not nil - it would ignore updated when a user is removed from the group
-			if addedUsers.NewChatMembers != nil {
-				for _, v := range newUser {
-					if v["username"] != "" {
-						for _, vv := range byt {
-							//check if the user have not been added to the group before
-							if vv["username"] != v["username"] {
-								//add the new user
-								insertID, _ := database.CreateMongoDoc(database.UserCollection, addedUsers)
-								// if err != nil {
-								// 	fmt.Printf("Mongo db error: %v\n", err)
-								// }
-								fmt.Printf("Mongodb data created with ID: %v\n", insertID)
+				// addUserUpdate := update.Message.NewChatMembers
+				// for i, v := range addUserUpdate {
+				// 	if i == 0 {
+				// 		fmt.Println(v)
+				// 		addUser = true
+				// 	}
+				// }
+				// var addedUser []map[string]string
+				// aUserJson, _ := json.Marshal(addUserUpdate)
+				// json.Unmarshal(aUserJson, &addedUser)
+				// for val := range addedUser {
+				// 	username := addedUser[val]["username"]
+				// 	addUser = username
+				// }
+
+				// 	if removeUser == "" {
+				// 		// check the database to get the number of users a particular user have added
+				// 		// to the group to know if they are eligible to send messages to the group in this case 30
+				// 		countFilter := bson.M{"from.username": update.Message.From.UserName}
+				// 		addedUserCount := database.CountCollection(database.UserCollection, countFilter)
+				// 		if addedUserCount < 2 {
+				// 			//delete the messages sent to the group by the user
+				// 			deleteMsg := tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID)
+				// 			bot.Send(deleteMsg)
+
+				// 			fmt.Println(deleteMsg)
+				// 			//notify the users that that they need to add 30 people to the group
+				// 			sendMsg := fmt.Sprintf("@%s you need to add 30 users to be able to send messages to this group", foundUser)
+				// 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, sendMsg)
+				// 			// msg.ReplyToMessageID = update.Message.MessageID
+				// 			bot.Send(msg)
+				// 		}
+				// 	}
+				// }
+
+				//check if the recently added user already exists in the datatase
+				filter := bson.M{"from.username": update.Message.From.UserName}
+				res, _ := database.GetMongoDoc(database.UserCollection, filter)
+				if res != nil {
+					//loop through the object to get the username of the just added user
+					var byt []map[string]interface{}
+					arr := res.NewChatMembers
+					jm, _ := json.Marshal(arr)
+
+					json.Unmarshal(jm, &byt)
+
+					//add _ number users to the group before being able to send messages to the group
+					up := update.Message
+					var addedUsers database.AddedUsers
+					jsonr, _ := json.Marshal(up)
+
+					json.Unmarshal(jsonr, &addedUsers)
+
+					var newUser []map[string]interface{}
+					addedUserR := addedUsers.NewChatMembers
+					addedUserjson, _ := json.Marshal(addedUserR)
+
+					json.Unmarshal(addedUserjson, &newUser)
+
+					//so long newchatmembers object is not nil - it would ignore updated when a user is removed from the group
+					if addedUsers.NewChatMembers != nil {
+						for v := range newUser {
+							if newUser[v]["username"] != "" {
+								for vv := range byt {
+									//check if the user have not been added to the group before
+									if byt[vv]["username"] != newUser[v]["username"] {
+										//add the new user
+										insertID, _ := database.CreateMongoDoc(database.UserCollection, addedUsers)
+										// if err != nil {
+										// 	fmt.Printf("Mongo db error: %v\n", err)
+										// }
+										fmt.Printf("Mongodb data created with ID: %v\n", insertID)
+									}
+								}
 							}
 						}
 					}
-				}
-			}
+				} else {
+					up := update.Message
+					var addedUsers database.AddedUsers
+					jsonr, _ := json.Marshal(up)
 
-			//delete messages that contains link sent by other users aside from the admin
-			adminUser := update.Message.From.UserName
-			if adminUser != os.Getenv("USER_NAME") {
+					json.Unmarshal(jsonr, &addedUsers)
+					insertID, _ := database.CreateMongoDoc(database.UserCollection, addedUsers)
+					fmt.Printf("Mongodb data created with ID: %v\n", insertID)
+				}
+
+				//delete messages that contains link sent by other users aside from the admin
 				domain := urlChecker(update.Message.Text)
 				if domain {
 					fmt.Println("Message contains a link...")
