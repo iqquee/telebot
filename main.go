8003239a1fa3013a91385e2cd6f90e5c38dac2f7
hisyntax <hisyntax0@gmail.com>
2022-09-04 11:03:31 +0100

    new request handler creater to hold the bot function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	// "github.com/gorilla/mux"
 
+	"github.com/gorilla/mux"
 	"github.com/hisyntax/telebot/tele"
 	"github.com/joho/godotenv"
 )
@@ -13,6 +17,18 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println(".env not found")
 	}
+	r := mux.NewRouter()
 
+	for {
+		fmt.Printf("for loop running...\n")
+		r.HandleFunc("/", RunBot)
+
+		port := os.Getenv("PORT")
+
+		http.ListenAndServe(":"+port, r)
+	}
+}
+
+func RunBot(w http.ResponseWriter, r *http.Request) {
 	tele.Bot()
 }
